73ecb5c255b37ea06c225b03e3f7525ba00463e3
Jizhou Yang <yangy@metropolia.fi>
2019-10-06 15:25:46 +0300

    Add API stub

---
+++ api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from flask import Flask
+from flask_restful import Resource, Api
+
+import random
+
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+class Spam(Resource):
+    def post(self):
+        # Placeholder functionality
+        # Just returns true/false randomly
+        answer = bool(random.getrandbits(1))
+        return {'spam': answer}
+
+api.add_resource(HelloWorld, '/hello')
+api.add_resource(Spam, '/spam')
+
+if __name__ == '__main__':
+    app.run(debug=True)
