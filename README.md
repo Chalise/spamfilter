9e1de650a4375162ca0f2d6fe6d99b4aaa352a12
Jizhou Yang <yangy@metropolia.fi>
2019-10-06 19:04:30 +0300

    API and GUI stubs

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # spamfilter
 Machine-learning spam filter
+
+API implemented in Flask-Restful, python3. Served with gunicorn.
+
+API in debug mode:
+$ python3 api.py
+
+API with gunicorn:
+$ gunicorn --bind 0.0.0.0 api.api:app
+
+With gunicorn you can then try:
+curl -X POST http://127.0.0.1:8000/spam
---
+++ ui/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios';
+
+class App extends Component {
+    constructor () {
+        super()
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick () {
+          axios.get('https://api.github.com/users/maecapozzi')
+	    .then(response => console.log(response));
+    }
+
+    render () {
+        return (
+            <div className='button__container'>
+                <button className='button' onClick={this.handleClick}>
+                    Click Me
+                </button>
+	    </div>
+	)
+    }
+}
+
+export default App
