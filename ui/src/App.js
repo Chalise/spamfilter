020d660ab39a69da101f627972e235cf2571b614
Jizhou Yang <jizhou.yang@ssh.com>
2019-10-15 15:58:45 +0300

    Recreate UI

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,27 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import axios from 'axios';
 
-class App extends Component {
-    constructor () {
-        super()
-        this.handleClick = this.handleClick.bind(this)
-    }
-
-    handleClick () {
-          axios.get('https://api.github.com/users/maecapozzi')
-	    .then(response => console.log(response));
-    }
-
-    render () {
-        return (
-            <div className='button__container'>
-                <button className='button' onClick={this.handleClick}>
-                    Click Me
-                </button>
-	    </div>
-	)
-    }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
