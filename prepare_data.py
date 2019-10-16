6075c33de7b95496d5d4db124b4994ab68d54c33
saraalka <saraalkafri@gmail.com>
2019-10-16 14:31:42 +0300

    file with prepare data function

---
+++ prepare_data.py
@@ -0,0 +1,120 @@
+import os
+import re
+
+# import these modules
+from itertools import count
+
+import numpy as np
+from nltk import PorterStemmer, WordNetLemmatizer
+from nltk.tokenize import SpaceTokenizer
+
+from html.parser import HTMLParser
+
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.strict = False
+        self.convert_charrefs = True
+        self.fed = []
+
+    def handle_data(self, d):
+        self.fed.append(d)
+
+    def get_data(self):
+        return ''.join(self.fed)
+
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
+
+
+def remove_html_tags(text):
+    # Remove html tags from a string
+    clean = re.compile(r'<.*?>')
+    return re.sub(clean, '', text)
+
+
+def stemming_words(stemtext):
+    ps = PorterStemmer()
+    return ps.stem(stemtext)
+    # Stemming words with NLTK
+
+
+def tokenize_space(tokentext):
+    # Create a reference variable for Class SpaceTokenizer
+    tk = SpaceTokenizer()
+    # Create a string input
+    # Use tokenize method
+    return tk.tokenize(tokentext)
+
+
+def findURL(urltext):
+    # findall() has been used
+    # with valid conditions for urls in string
+    url = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+] |[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', urltext)
+    if url:
+        return 'httpaddr'
+    else:
+        return urltext
+
+
+def findNumber(num):
+    numb = re.findall("[^a-zA-Z:][-+]?\d+[\.]?\d*", num.__str__().__str__())
+
+    if numb:
+        return "number"
+    else:
+        return num
+
+
+def findemail(urltext):
+    email = re.findall('^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$', urltext)
+    if email:
+        return 'emailaddr'
+    else:
+        return urltext
+
+
+# Load and concatenate all the emails in one string
+def prepare_data(emailtext):
+    content = []
+    i = 0
+    for line in emailtext.splitlines():
+        line = strip_tags(line)  # Stripping HTML
+        for word in line.split():
+            # Lower-casing
+            word = word.lower()
+            # Word Stemming
+            word = stemming_words(word)
+            # Normalizing URL
+            word = findURL(word)
+            # Normalizing Email Addresses
+            word = findemail(word)
+            # Removal of non-words
+            word = re.compile('\w+').findall(word)
+            # word = findNumber(word)
+            for i in word:
+                content.append(i)
+    print(content)
+    return content
+
+
+
+
+def build_features(dictionary):
+    # Read the file names
+    d = {}
+    # ndarray to have the features
+    features_matrix = np.zeros((1, len(dictionary)))
+    for word in dictionary:
+        d[word] = d.get(word, 0) + 1
+    print(d)
+    return d
+
+
+dictionary = prepare_data("<!DOCTYPE > <html><body> <h1>My First Heading</h1> <p>My first paragraph.???</p><p> Find "
+                          "the link")
+build_features(dictionary)
--- spamfilter1.py
+++ spamfilter1.py
@@ -8,6 +8,9 @@ import random
 
 
 #defines dictonary function
+import prepare_data
+
+
 def dictionary(words):
     # creates list for dictionary words
     spamdict = dict([(word, True) for word in words])
@@ -16,27 +19,23 @@ def dictionary(words):
 
 # This function is going to teach naive bayes to differentiate spam and non-spma
 def get_classifier():
-
     training_data1 = "spamdata/nonspam-train"
-
     nonspam_list = []
     combined_list = []
-    
+
     # for loop that goes through training files for non-spam
     for directories, subdirs, files in os.walk(training_data1):
-    for filename in files:
-        with open(os.path.join(directories,filename), encoding = "latin-1") as f:
-            #reads throuh the files and puts strings in data object
-            data = f.read()
-            # word_tokenize makes strings to separated words and puts list to object words
-            words = word_tokenize(data)
-            # words are added to nonspam list and dictionary
-            nonspam_list.extend((dictionary(words), 'nonspam'))
-            
+        for filename in files:
+            with open(os.path.join(directories,filename), encoding = "latin-1") as f:
+                #reads throuh the files and puts strings in data object
+                data = f.read()
+                # word_tokenize makes strings to separated words and puts list to object words
+                words = word_tokenize(data)
+                # words are added to nonspam list and dictionary
+                nonspam_list.extend((dictionary(words), 'nonspam'))
 
     training_data2 = "spamdata/spam-train"
     spam_list = []
-
     # for loop that goes through training files for spam
     for directories, subdirs, files in os.walk(training_data2):
         for filename in files:
@@ -47,9 +46,8 @@ def get_classifier():
                 words = word_tokenize(data)
                 # words are added to spam list and dictionary
                 nonspam_list.extend((dictionary(words), 'spam'))
-           
-    print(nonspam_list[0])
-    print(spam_list[0])
+    #print(nonspam_list[0])
+    #print(spam_list[0])
 
     #compined spam and nonspam lists for training
     combine_list = {[nonspam_list]+[spam_list]}
@@ -64,21 +62,22 @@ def get_classifier():
     #counts classifiers accurasy using testing set
     Accuracy = nltk.classify.util.accuracy(classifier, test_set)
     print("Accuracy is: ", Accuracy * 100)
-
     #returns 
     return classifier
 
 # function for clients emails for real time checks
-def realtimeclassification(classifier):
-    #gets testdata files
-    test_set = 'spamdata/testing_set'
-    
-    words = 
+def realtimeclassification(classifier, email):
+    test_set = "spamdata/Testing_set"
+    words = prepare_data.prepare_data(email)
     feature = dictionary(words)
-    return("Message is: " , classifier.classify(feature))
-
-    #Counts resluts accuracy using testdata files
     Accuracy = nltk.classify.util.accuracy(classifier, test_set)
     print("Accuracy is: ", Accuracy * 100)
+   # return ("Message is: ", classifier.classify(feature))
+    return 'true'
+# Train the classifier
+# get the client email
+# Run realtimeclassification.. return spam
+
+classifierTest = get_classifier()
     
 
