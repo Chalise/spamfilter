a366568bd318cc8e06bd9933c1f71543d5fa244f
saraalka <saraalkafri@gmail.com>
2019-11-13 18:52:45 +0200

    add comments to code

--- backend/prepare_data.py
+++ backend/prepare_data.py
@@ -24,26 +24,46 @@ class MLStripper(HTMLParser):
     def get_data(self):
         return ''.join(self.fed)
 
-
 def strip_tags(html):
     s = MLStripper()
     s.feed(html)
     return s.get_data()
 
 
+
 def remove_html_tags(text):
-    # Remove html tags from a string
+    """
+    All HTML tags are removed from the emails.
+    Many emails often come with HTML formatting; we remove all the
+    HTML tags, so that only the content remains.
+    :param text:
+    :return: string without html tags
+    """
+
     clean = re.compile(r'<.*?>')
     return re.sub(clean, '', text)
 
 
+
 def stemming_words(stemtext):
+    """
+    Words are reduced to their stemmed form. For example,
+        “discount”, “discounts”, “discounted” and “discounting” are all
+        replaced with “discount”.
+    :param stemtext:
+    :return:  Stemmed words with NLTK
+    """
     ps = PorterStemmer()
     return ps.stem(stemtext)
-    # Stemming words with NLTK
+
 
 
 def tokenize_space(tokentext):
+    """
+
+    :param tokentext:
+    :return: tokenized text
+    """
     # Create a reference variable for Class SpaceTokenizer
     tk = SpaceTokenizer()
     # Create a string input
@@ -52,6 +72,11 @@ def tokenize_space(tokentext):
 
 
 def findURL(urltext):
+    """
+    All URLs are replaced with the text “httpaddr”.
+    :param urltext:
+    :return: “httpaddr" if it is a URL
+    """
     # findall() has been used
     # with valid conditions for urls in string
     url = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+] |[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', urltext)
@@ -62,6 +87,12 @@ def findURL(urltext):
 
 
 def findNumber(num):
+    """
+    All numbers are replaced with the text
+    “number”.
+    :param num:
+    :return: “number" if it is a number
+    """
     numb = re.findall("[^a-zA-Z:][-+]?\d+[\.]?\d*", num)
 
     if numb:
@@ -71,6 +102,12 @@ def findNumber(num):
 
 
 def findemail(urltext):
+    """
+    All numbers are replaced with the text
+    “number”.
+    :param urltext:
+    :return: “emailaddr" if it is a email address
+    """
     email = re.findall('^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$', urltext)
     if email:
         return 'emailaddr'
